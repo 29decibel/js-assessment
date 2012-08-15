9850f16628edc1dfdc7855e9fb7554f45ba2e78d
dongbin li <mike.d.1984@gmail.com>
2012-08-15 15:53:34 +0800

    clear async tests

--- app/async.js
+++ app/async.js
@@ -3,11 +3,34 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+       //return a object,
+       //has then funciton
+       var delayed = {};
+       delayed.then = function(callback){
+         callback.call(delayed,value);
+       }
+       return delayed;
     },
 
     manipulateRemoteData : function(url) {
-
+       //return a defered object
+       //has a then method
+       var delayed = {};
+       // just store the callback
+       delayed.then = function(callback){
+         this.callback = callback;
+       }
+       delayed.working = function(url){
+         var that = this;
+         $.get('http://localhost:4444'+url,function(result){
+           var r = $.map(result.people,function(data,index){
+             return data.name;
+           })
+           that.callback(r);
+         })
+       }
+       delayed.working(url);
+       return delayed;
     }
   };
 });
